221eb36d020e315564f8bdc4701b6f9a4243007d
Joe Richey <joerichey@google.com>
2019-10-04 23:11:32 -0700

    raw/structs: Use generated Trait impls

--- src/raw/structs.rs
+++ src/raw/structs.rs
@@ -4,6 +4,7 @@ use super::{CommandData, ResponseData, Tpm};
 use crate::Result;
 
 // Header for all commands (see v1.55, Part 1, Section 18)
+#[derive(CommandData)]
 pub(crate) struct CommandHeader {
     pub tag: tag::Command,
     pub size: u32,
@@ -11,6 +12,7 @@ pub(crate) struct CommandHeader {
 }
 
 // Header for all respsonses (see v1.55, Part 1, Section 18)
+#[derive(ResponseData)]
 pub(crate) struct ResponseHeader {
     pub tag: tag::Command,
     pub size: u32,
@@ -18,7 +20,7 @@ pub(crate) struct ResponseHeader {
 }
 
 // TPMS_CLOCK_INFO (v1.55, Part 2, Section 10.11.1, Table 116)
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, ResponseData)]
 pub(crate) struct ClockInfo {
     clock: u64,
     reset_count: u32,
@@ -27,52 +29,8 @@ pub(crate) struct ClockInfo {
 }
 
 // TPMS_TIME_INFO (v1.55, Part 2, Section 10.11.6, Table 117)
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, ResponseData)]
 pub struct TimeInfo {
     time: u64,
     clock: ClockInfo,
-}
-
-// GENERATED CODE BELOW
-
-impl ResponseData for ClockInfo {
-    fn decode(reader: &mut (impl Tpm + ?Sized)) -> Result<Self> {
-        Ok(Self {
-            clock: u64::decode(reader)?,
-            reset_count: u32::decode(reader)?,
-            restart_count: u32::decode(reader)?,
-            safe: bool::decode(reader)?,
-        })
-    }
-}
-
-impl ResponseData for TimeInfo {
-    fn decode(reader: &mut (impl Tpm + ?Sized)) -> Result<Self> {
-        Ok(Self {
-            time: u64::decode(reader)?,
-            clock: ClockInfo::decode(reader)?,
-        })
-    }
-}
-
-impl CommandData for CommandHeader {
-    fn encoded_len(&self) -> usize {
-        0 + self.tag.encoded_len() + self.size.encoded_len() + self.code.encoded_len()
-    }
-    fn encode(&self, writer: &mut (impl Tpm + ?Sized)) -> Result<()> {
-        self.tag.encode(writer)?;
-        self.size.encode(writer)?;
-        self.code.encode(writer)?;
-        Ok(())
-    }
-}
-
-impl ResponseData for ResponseHeader {
-    fn decode(reader: &mut (impl Tpm + ?Sized)) -> Result<Self> {
-        Ok(Self {
-            tag: tag::Command::decode(reader)?,
-            size: u32::decode(reader)?,
-            code: ResponseCode::decode(reader)?,
-        })
-    }
 }
