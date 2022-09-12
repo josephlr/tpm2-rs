52a0a95324f7d7587e36f6edd677b6e75896547d
Joe Richey <joerichey@google.com>
2022-09-12 04:05:01 -0700

    Fixup formatting nits
    Signed-off-by: Joe Richey <joerichey@google.com>

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -4,14 +4,14 @@ pub trait CommandData {
     fn marshal_handles(&self, _: &mut &mut [u8]) -> Result<()> {
         Ok(())
     }
-    fn marshal_params(&self, _: &mut &mut [u8]) -> Result<()>;
+    fn marshal_params(&self, buf: &mut &mut [u8]) -> Result<()>;
 }
 
 pub trait ResponseData<'a> {
     fn unmarshal_handles(&mut self, _: &mut &[u8]) -> Result<()> {
         Ok(())
     }
-    fn unmarshal_params(&mut self, _: &mut &'a [u8]) -> Result<()>;
+    fn unmarshal_params(&mut self, buf: &mut &'a [u8]) -> Result<()>;
 }
 
 impl ResponseData<'_> for () {
