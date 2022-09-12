9e0ba4eda69c762b6ac0f4604c993b2dd8061ea8
Joe Richey <joerichey@google.com>
2022-09-11 22:21:45 -0700

    Rework main Tpm structs and traits
    Now instead of having a Driver trait and a Tpm struct, we just have the
    Tpm trait, which includes the previous Driver functionality, as well as
    buffer management.
    
    The run, and run_with_auth methods are now just `dyn Tpm` methods,
    allowing them to be _called_ on any object that implements `Tpm`, but
    not to be overridden.
    
    This also finishes the implementation for processing command/response
    authorizations.
    
    Signed-off-by: Joe Richey <joerichey@google.com>

--- src/lib.rs
+++ src/lib.rs
@@ -25,22 +25,22 @@ use types::*;
 pub type Handle = u32;
 
 /// The main interface to a TPM2 device.
-pub struct Tpm {
-    buf: Box<[u8]>,
-    driver: Box<dyn Driver>,
+pub trait Tpm {
+    fn command_buf(&mut self) -> &mut [u8];
+    fn response_buf(&self) -> &[u8];
+    fn execute_command(&mut self, cmd_size: u32) -> Result<u32>;
 }
 
-impl<'a> Tpm {
+impl dyn Tpm + '_ {
     #[inline]
-    pub fn exec<C: Command>(&'a mut self, cmd: C) -> Result<C::Response<&'a [u8]>>
+    pub fn run<'a, C: Command>(&'a mut self, cmd: C) -> Result<C::Response<&'a [u8]>>
     where
         C::Response<&'a [u8]>: Unmarshal<'a>,
     {
-        self.exec_with_auths(cmd, &[])
+        self.run_with_auths(cmd, &[])
     }
 
-    #[inline]
-    pub fn exec_with_auths<C: Command>(
+    pub fn run_with_auths<'a, C: Command>(
         &'a mut self,
         cmd: C,
         auths: &[&dyn Auth],
@@ -50,9 +50,8 @@ impl<'a> Tpm {
     {
         let mut rsp: C::Response<&'a [u8]> = Default::default();
         let mut rsp_handles = <C::Response<&'a [u8]> as Response>::Handles::empty();
-        exec_impl(
-            self.driver.as_mut(),
-            &mut self.buf,
+        run_impl(
+            self,
             C::CODE,
             cmd.auth_handles().as_slice(),
             cmd.handles().as_slice(),
@@ -66,19 +65,106 @@ impl<'a> Tpm {
     }
 }
 
-#[allow(unused_variables)]
-fn exec_impl(
-    driver: &mut dyn Driver,
-    buf: &mut [u8],
+fn run_impl<'a>(
+    tpm: &'a mut dyn Tpm,
     code: tpm::CC,
     auth_handles: &[AuthHandle],
-    handles: &[Handle],
+    cmd_handles: &[Handle],
     extra_auths: &[&dyn Auth],
-    params: &dyn Marshal,
+    cmd_params: &dyn Marshal,
     rsp_handles: &mut [Handle],
-    rsp_params: &mut dyn Unmarshal,
+    rsp_params: &mut dyn Unmarshal<'a>,
 ) -> Result<()> {
-    todo!()
+    let has_auths = !auth_handles.is_empty() || !extra_auths.is_empty();
+
+    //// Marshal Command
+    let mut cmd_buf = tpm.command_buf();
+    let buf_len = cmd_buf.len();
+    // Marshal the header at the end
+    let header_buf: &mut [u8];
+    (header_buf, cmd_buf) = cmd_buf.split_at_mut(CommandHeader::SIZE);
+
+    // Marshal Handles
+    for auth_handle in auth_handles {
+        auth_handle.handle.marshal(&mut cmd_buf)?;
+    }
+    for handle in cmd_handles {
+        handle.marshal(&mut cmd_buf)?;
+    }
+
+    // Marshal Authorization Area
+    if has_auths {
+        assert!(auth_handles.len() + extra_auths.len() <= 3);
+        // Marshal auth size at the end
+        let auth_size_buf: &mut [u8];
+        (auth_size_buf, cmd_buf) = cmd_buf.split_at_mut(u32::SIZE);
+        let cmd_buf_len = cmd_buf.len();
+
+        for auth_handle in auth_handles {
+            auth_handle.auth.get_auth().marshal(&mut cmd_buf)?;
+        }
+        for auth in extra_auths {
+            auth.get_auth().marshal(&mut cmd_buf)?;
+        }
+
+        let auth_size: u32 = (cmd_buf_len - cmd_buf.len()).try_into().unwrap();
+        auth_size.marshal_exact(auth_size_buf)?;
+    }
+
+    // Marshal Parameters
+    cmd_params.marshal(&mut cmd_buf)?;
+
+    // Marshal Header
+    let cmd_header = CommandHeader {
+        tag: if has_auths {
+            tpm::ST::Sessions
+        } else {
+            tpm::ST::NoSessions
+        },
+        size: (buf_len - cmd_buf.len()).try_into().unwrap(),
+        code,
+    };
+    cmd_header.marshal_exact(header_buf)?;
+
+    //// Execute the command
+    let rsp_len = tpm.execute_command(cmd_header.size)?;
+
+    //// Unmarshal Response
+    let mut rsp_buf: &'a [u8] = &tpm.response_buf()[..rsp_len.to_usize()];
+    let rsp_header = ResponseHeader::unmarshal_val(&mut rsp_buf)?;
+
+    // Check for errors
+    assert!(rsp_header.size == rsp_len);
+    if let Some(err_code) = NonZeroU32::new(rsp_header.code.0) {
+        return Err(Error::Tpm(err_code));
+    }
+    assert!(rsp_header.tag == cmd_header.tag);
+
+    // Unmarshal Handles
+    for handle in rsp_handles {
+        handle.unmarshal(&mut rsp_buf)?;
+    }
+
+    // Unmarshal Authorization Area
+    if has_auths {
+        let param_size = u32::unmarshal_val(&mut rsp_buf)?;
+        let mut auth_buf: &[u8];
+        (rsp_buf, auth_buf) = rsp_buf.split_at(param_size.try_into().unwrap());
+
+        let mut auth_rsp = tpms::AuthResponse::default();
+        for auth_handle in auth_handles {
+            auth_rsp.unmarshal(&mut auth_buf)?;
+            auth_handle.auth.set_auth(&auth_rsp)?;
+        }
+        for auth in extra_auths {
+            auth_rsp.unmarshal(&mut auth_buf)?;
+            auth.set_auth(&auth_rsp)?;
+        }
+        assert!(auth_buf.is_empty());
+    }
+
+    // Unmarshal Parameters
+    rsp_params.unmarshal_exact(rsp_buf)
 }
 
 #[cfg(test)]
@@ -90,17 +176,17 @@ mod test {
     #[test]
     fn can_exec() {
         #[allow(dead_code)]
-        fn take_tpm(tpm: &mut Tpm) -> Result<Vec<u8>> {
-            tpm.exec(Startup {
+        fn take_tpm(tpm: &mut dyn Tpm) -> Result<Vec<u8>> {
+            tpm.run(Startup {
                 startup_type: tpm::SU::Clear,
             })?;
 
-            let rsp = tpm.exec(GetRandom {
+            let rsp = tpm.run(GetRandom {
                 bytes_requested: 12,
             })?;
             let b = Vec::from(rsp.random_bytes);
 
-            tpm.exec(Shutdown {
+            tpm.run(Shutdown {
                 shutdown_type: tpm::SU::Clear,
             })?;
             Ok(b)
