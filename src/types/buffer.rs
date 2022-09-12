9cdccbe5606b6ec3b4a07a25ffa2f4ba5a2e4686
Joe Richey <joerichey@google.com>
2022-09-11 21:49:58 -0700

    Move Buffer impl for Vec into submodule
    Signed-off-by: Joe Richey <joerichey@google.com>

--- src/types/buffer.rs
+++ src/types/buffer.rs
@@ -1,5 +1,5 @@
 use super::{marshal_slice, unmarshal_slice, Marshal, Unmarshal};
-use crate::{Result, Vec};
+use crate::Result;
 use core::fmt::Debug;
 
 pub trait Buffer: Marshal + Default + Debug {}
@@ -23,20 +23,26 @@ impl<'a> Unmarshal<'a> for &'a [u8] {
 
 impl<'a> Buffer for &'a [u8] {}
 
-impl Marshal for Vec<u8> {
-    fn marshal(&self, buf: &mut &mut [u8]) -> Result<()> {
-        self.as_slice().marshal(buf)
-    }
-}
+#[cfg(feature = "alloc")]
+mod vec_buffer {
+    use super::*;
+    use alloc::vec::Vec;
 
-impl Unmarshal<'_> for Vec<u8> {
-    fn unmarshal(&mut self, buf: &mut &[u8]) -> Result<()> {
-        let len: usize = u16::unmarshal_val(buf)?.into();
-        let data = unmarshal_slice(len, buf)?;
-        self.clear();
-        self.extend_from_slice(data);
-        Ok(())
+    impl Marshal for Vec<u8> {
+        fn marshal(&self, buf: &mut &mut [u8]) -> Result<()> {
+            self.as_slice().marshal(buf)
+        }
     }
-}
 
-impl Buffer for Vec<u8> {}
+    impl Unmarshal<'_> for Vec<u8> {
+        fn unmarshal(&mut self, buf: &mut &[u8]) -> Result<()> {
+            let len: usize = u16::unmarshal_val(buf)?.into();
+            let data = unmarshal_slice(len, buf)?;
+            self.clear();
+            self.extend_from_slice(data);
+            Ok(())
+        }
+    }
+
+    impl Buffer for Vec<u8> {}
+}
