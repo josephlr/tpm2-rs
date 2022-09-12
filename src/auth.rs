7497f40acea773a9e813b5328768d34db784169c
Joe Richey <joerichey@google.com>
2022-09-12 00:02:51 -0700

    Add PasswordAuth and Handle -> AuthHandle conversion
    Signed-off-by: Joe Richey <joerichey@google.com>

--- src/auth.rs
+++ src/auth.rs
@@ -1,4 +1,4 @@
-use crate::{tpms, Handle, Result};
+use crate::{tpm, tpma, tpms, Handle, Result};
 
 pub trait Auth: core::fmt::Debug {
     fn get_auth(&self) -> tpms::AuthCommand;
@@ -11,6 +11,43 @@ pub struct AuthHandle<'a> {
     pub auth: &'a dyn Auth,
 }
 
+#[derive(Debug)]
+pub struct PasswordAuth<'a>(&'a [u8]);
+
+const CONTINUE_SESSION: tpma::Session = {
+    let mut s = tpma::Session::empty();
+    s.continue_session = true;
+    s
+};
+
+impl Auth for PasswordAuth<'_> {
+    fn get_auth(&self) -> tpms::AuthCommand {
+        tpms::AuthCommand {
+            session_handle: tpm::rh::PASSWORD,
+            nonce: &[],
+            session_attributes: CONTINUE_SESSION,
+            hmac: self.0,
+        }
+    }
+
+    fn set_auth(&self, auth: &tpms::AuthResponse) -> Result<()> {
+        assert!(auth.nonce.is_empty());
+        assert_eq!(auth.session_attributes, CONTINUE_SESSION);
+        assert!(auth.hmac.is_empty());
+        Ok(())
+    }
+}
+
+/// Convert a handle to an AuthHandle with Password Authorization
+impl From<Handle> for AuthHandle<'_> {
+    fn from(handle: Handle) -> Self {
+        AuthHandle {
+            handle,
+            auth: &PasswordAuth(&[]),
+        }
+    }
+}
+
 pub trait AuthHandleSlice {
     fn empty() -> Self;
     fn as_slice(&self) -> &[AuthHandle];
@@ -18,7 +55,8 @@ pub trait AuthHandleSlice {
 
 impl<const N: usize> AuthHandleSlice for [AuthHandle<'_>; N] {
     fn empty() -> Self {
-        todo!()
+        let ah: AuthHandle = (0 as Handle).into();
+        [ah; N]
     }
     fn as_slice(&self) -> &[AuthHandle] {
         self
