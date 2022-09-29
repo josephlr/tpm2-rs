7acfce7ef6ab4b214447d71e921b3bc81eb05ea8
Joe Richey <joerichey@google.com>
2022-09-28 21:40:24 -0700

    Update examples to use new trait methods
    Signed-off-by: Joe Richey <joerichey@google.com>

--- examples/getrandom.rs
+++ examples/getrandom.rs
@@ -1,18 +1,40 @@
-use tpm2::{commands::GetRandom, os::get_default_tpm, Command};
+use tpm2::{commands::GetRandom, os::get_default_tpm, Tpm};
 
 const NUM_BYTES: u16 = 100;
 
 fn main() {
     let mut tpm = get_default_tpm().expect("Unable to open TPM");
 
+    run_example(&mut tpm);
+    ext_example(&mut tpm);
+}
+
+fn run_example(tpm: &mut dyn Tpm) {
+    use tpm2::Run;
+
+    println!("Requesting {} random bytes with one command", NUM_BYTES);
     let cmd = GetRandom {
         bytes_requested: NUM_BYTES,
     };
-    println!("Requesting {} random bytes from the TPM", NUM_BYTES);
-
-    let rsp = cmd.run(&mut tpm).expect("TPM2_GetRandom failed");
+    let rsp = tpm.run(&cmd).expect("TPM2_GetRandom failed");
     println!("Got {} random bytes:", rsp.random_bytes.len());
-    for chunk in rsp.random_bytes.chunks(8) {
+    print_bytes(rsp.random_bytes);
+}
+
+fn ext_example(tpm: &mut dyn Tpm) {
+    use tpm2::TpmExt;
+
+    println!(
+        "Requesting {} random bytes with multiple commands",
+        NUM_BYTES
+    );
+    let mut buf = [0u8; NUM_BYTES as usize];
+    tpm.getrandom(&mut buf).expect("getrandom failed");
+    print_bytes(&buf);
+}
+
+fn print_bytes(b: &[u8]) {
+    for chunk in b.chunks(8) {
         println!("\t{:02X?}", chunk)
     }
 }
--- examples/read_pcrs.rs
+++ examples/read_pcrs.rs
@@ -2,7 +2,7 @@ use tpm2::{
     commands::PcrRead,
     os::get_default_tpm,
     types::{tpm, tpml, tpms},
-    Command,
+    Run,
 };
 
 fn main() {
@@ -25,7 +25,7 @@ fn main() {
         pcr_selection: tpml::PcrSelection::from(&sel),
     };
     println!("Reading from {:?} PCR banks", cmd.pcr_selection.len());
-    let rsp = cmd.run(&mut tpm).expect("Unable to read PCRS");
+    let rsp = tpm.run(&cmd).expect("Unable to read PCRS");
 
     for sel in rsp.pcr_selection {
         print!("{:?} selection:", sel.hash);
