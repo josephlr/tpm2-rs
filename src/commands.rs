91b9dfe2254f8132c334fcd3bf0ff7b4533aa52f
Joe Richey <joerichey@google.com>
2022-10-02 01:41:51 -0700

    Move commands to file instead of directory
    Signed-off-by: Joe Richey <joerichey@google.com>

--- src/commands.rs
+++ src/commands.rs
@@ -1,3 +1,15 @@
+//! TPM2 [`Command`]s and Responses
+//!
+//! These structures should be fairly direct translations of the
+//! "TPM2_* Command" and "TPM2_* Response" tables in Part 3 of the TPM2 Spec.
+//!
+//! Of the 117 TPM2 commands, 5 are implemented.
+//! If a command is not implemented, there will be a skeleton of code and doc
+//! comments which are commented out.
+//!
+//! TODO:
+//!   - Add additional notes about TPM2_HMAC and TPM2_StartHMAC
+
 use crate::{
     error::{MarshalError, UnmarshalError},
     marshal::{CommandData, ResponseData},
