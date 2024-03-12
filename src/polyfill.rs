cfaf9d8363f4c295d672ff7fe1a908e38f35d520
Tim Wawrzynczak <tim@rivosinc.com>
2024-03-12 10:29:53 -0600

    polyfill: Add ToMutArr trait

--- src/polyfill.rs
+++ src/polyfill.rs
@@ -3,13 +3,24 @@
 use core::mem::size_of;
 const _: () = assert!(size_of::<usize>() >= 4, "usize must be at least 32 bits");
 
+pub(crate) trait ToMutArr<const N: usize> {
+    fn to_mut_arr(&mut self) -> &mut [u8; N];
+}
+
+impl<const N: usize> ToMutArr<N> for [u8] {
+    #[inline(always)]
+    fn to_mut_arr(&mut self) -> &mut [u8; N] {
+        self.try_into().unwrap()
+    }
+}
+
 pub(crate) trait ToArr<const N: usize> {
-    fn to_arr(&mut self) -> &mut [u8; N];
+    fn to_arr(&self) -> &[u8; N];
 }
 
 impl<const N: usize> ToArr<N> for [u8] {
     #[inline(always)]
-    fn to_arr(&mut self) -> &mut [u8; N] {
+    fn to_arr(&self) -> &[u8; N] {
         self.try_into().unwrap()
     }
 }
